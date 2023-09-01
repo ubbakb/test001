8c111d7ee11d25c67a69085398fb332b5433675d
ubbakb <141741369+ubbakb@users.noreply.github.com>
2023-09-01 11:26:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FWRuleDeciderTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _assembly = Assembly.GetExecutingAssembly();
+            var _stream = _assembly.GetManifestResourceStream("FWRuleDeciderTestConsole.EmbeddedResourceTest.txt");
+            string[] allResources = GetEmbeddedResourceNames();
+            
+            int a = 5;
+
+        }
+        public static Stream GetEmbeddedResourceStream(string resourceName)
+        {
+            return Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+        }
+        public static string[] GetEmbeddedResourceNames()
+        {
+            return System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceNames();
+        }
+    }
+}
