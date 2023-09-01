fdd75ca2aea26f81b3e7603ce46cf1bf7e1e4f7d
ubbakb <141741369+ubbakb@users.noreply.github.com>
2023-09-01 11:51:41 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Deneme sayisini gir: ");
+                int denemeSayisi = 0;
+                Int32.TryParse(Console.ReadLine(), out denemeSayisi);
+
+                List<int> items = new List<int>();
+                Random rd = new Random();
+
+                int erkeksayisi2 = 0;
+                for (int i = 0; i < denemeSayisi; i++)
+                {
+                    int a = rd.Next(0, 2);
+                    int b = rd.Next(0, 2);
+                    int c = rd.Next(0, 2);
+                    if (a + b == 2)
+                        erkeksayisi2 = erkeksayisi2 + 1;
+
+                    items.Add(a + b + c);
+                }
+
+                int erkeksayisi3 = items.Where(x => x == 3).Count();
+                //int erkeksayisi2 = items.Where(x => x == 2).Count() + erkeksayisi3;
+                float yuzde = ((float)erkeksayisi3 / (float)erkeksayisi2) * 100;
+                Console.WriteLine("2 Erkek sayisi (gerceklesmis ihtimal): " + erkeksayisi2.ToString());
+                Console.WriteLine("3 Erkek sayisi: " + erkeksayisi3.ToString());
+                Console.WriteLine("3 Erkek sayisi yüzde (3 erkek/2 erkek gerceklesmis ihtimal*100): " + yuzde.ToString());
+                Console.WriteLine("---------------------------------------------------------------------------------------");
+            }
+            
+        }
+    }
+}
